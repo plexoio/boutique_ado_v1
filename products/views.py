25f1362df858c9c62f3063735a0e5a3898c93539
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-08-14 20:39:42 +0000

    feat: display products & media queries for header

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -10,3 +10,16 @@ def all_products(request):
         'products': products
     }
     return render(request, 'products/products.html', context)
+
+
+def product_datail(request, product_id):
+    ''' Get the passed product view to diplay it using context'''
+
+    # 'pk' stands for primary key
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product
+    }
+    return render(request, 'products/product_detail.html', context)
