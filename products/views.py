fbe2d6d541e62479a05c4b3b8a17698d9be5dd3e
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-08-14 19:01:54 +0000

    feat: products view start

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    ''' A view to diplay all products, filtering & searching '''
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+    return render(request, 'products/products.html', context)
