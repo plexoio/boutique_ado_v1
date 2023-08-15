2e06bd4dffd8b0d5ded80afeb4143fb76b73f2c6
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-08-15 10:42:21 +0000

    feat: ability to search and queary add

--- products/views.py
+++ products/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 
 
 def all_products(request):
     ''' A view to diplay all products, filtering & searching '''
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+
+        # CATEGORY
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        # SEARCH ENGINE
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'No queries found!')
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context)
 
