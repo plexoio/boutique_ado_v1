2bc9f3cbbd4253f469d88f45e28514d2bccb9af3
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-08-16 18:03:40 +0000

    feat: shopping bag items display start

--- bag/views.py
+++ bag/views.py
@@ -7,7 +7,9 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    ''' For users to add desired product into the bag '''
+    ''' For users to add desired product into the bag.
+    This will fill the session with data to be used on the 'bag_contents'
+    definition at contexts.py'''
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -18,5 +20,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
